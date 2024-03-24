0c9eed68d2e4a636fcd30cce62d3b8ca3c58d63b
zannahwahyunilubis45 <zahraaprilianti04@students.amikom.ac.id>
2024-03-24 14:57:50 +0700

    first commit

---
+++ ObjectTaxi/Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTaxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool onDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", onDuty ? "Yes" : "No");
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}  
